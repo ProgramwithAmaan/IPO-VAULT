55fe6d8beb46ff28ff778ca2da5d413e825e80a9
Amaan Saifi <asaifi0625@gmail.com>
2026-09-13 12:48:46 +0530

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -25,7 +25,7 @@ const Header = ({ onAddClick, onExportExcel, onExportPDF }) => {
             {/* <div className="brand-dot" /> */}
               
       
-            <i className="bi bi-graph-up-arrow"></i>
+            <i class="bi bi-graph-up-arrow" style="color: red;"></i>
 
             
             <div>
